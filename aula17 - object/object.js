f502f7eec168a15ca778fabb14f1acdb0a504ef8
Gabriel Estevam <gabriel.estevam2204@gmail.com>
2023-06-07 20:22:44 -0300

    Fim do Curso de JavaScript pelo Curso em Video

---
+++ aula17 - object/object.js
@@ -0,0 +1,11 @@
+let amigo = {
+    nome: 'Jose',
+    sexo: 'Masculino',
+    peso: 85.5,
+    engordar(p=0){
+        console.log('Engordou')
+        this.peso += p
+    }
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
